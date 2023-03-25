9c00565f980ee54082767567c85ee5ed3ce02d61
Asandei Stefan <asandei.stefanel@gmail.com>
2023-03-25 13:06:15 +0200

    setup vite for vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import solid from "solid-start/vite";
+import vercel from "solid-start-vercel";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [
+    solid({
+      adapter: vercel(),
+    }),
+  ],
   ssr: { external: ["@prisma/client"] },
 });
