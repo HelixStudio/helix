c04bf76524fd6cd7d1308349d52ad25e8e488c08
Stefan Asandei <52822996+NikolaTesla13@users.noreply.github.com>
2023-05-24 14:18:58 +0300

    remove solidjs files

--- src/components/LoadingScreen.tsx
+++
@@ -1,7 +0,0 @@
-export default function LoadingScreen() {
-  return (
-    <div class="bg-secondary-800 text-white">
-      <p>Loading...</p>
-    </div>
-  );
-}
--- src/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "monaco-loader" {
-  export function useMonaco(): typeof import("monaco-editor");
-  //   export const MonacoLoader: import("solid-js").Component<{
-  //     children: import("solid-js").JSX.Element;
-  //   }>;
-}
