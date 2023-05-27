0f19006a86cf52dddddc433681dcb69105ae0d41
Asandei Stefan <asandei.stefanel@gmail.com>
2023-05-27 11:39:02 +0300

    fix font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import {
-  CloudArrowUpIcon,
   CommandLineIcon,
   CloudIcon,
   BookOpenIcon,
